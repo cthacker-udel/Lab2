01aed783a3f3f90b1a9aded7ee82d3c6126c92b4
cthacker-udel <cthacker@udel.edu>
2021-06-18 00:33:46 -0400

    Added 2 files, Othello.cpp and Player.cpp

---
+++ Lab2/Othello.cpp
@@ -0,0 +1,66 @@
+/*
+ * Othello.cpp
+ *
+ *  Created on: Jun 17, 2021
+ *      Author: Treah
+ */
+
+#include "Othello.h"
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+using namespace std;
+
+const char blank = '';
+const char pieceB = 'B';
+const char pieceW = 'O';
+
+void makemat(){
+	char mat[8][8] = {{'','','','','','','',''},{'','','','','','','',''},{'','','','','','','',''},
+			{'','','',pieceB,pieceW,'','',''},{'','','',pieceW,pieceB,'','',''},
+			{'','','','','','','',''},{'','','','','','','',''},{'','','','','','','',''}};
+
+
+}
+
+void printmat(){
+	cout<< makemat() << endl;
+}
+
+void Othello::playGame(){
+	makemat();
+	printmat();
+	bool play = true;
+	int fullsqrs = 0;
+	player p = p1;
+	bool whichp = true;
+	bool turn = true;
+	if (rand()%2 == 0) { // p1 plays first
+		p = p2;
+		turn = false;
+		whichp = false;
+	}
+
+	while ((play) && (fullsqrs < 64)){
+		cout << endl << p.name <<" ("<<p.piece<<") choose your square: "<<endl;
+		if ((numplayers == 2) || ((numplayers == 1) && turn)){
+		placepiece(p);
+		}
+		else if ((numplayers == 0) || ((numplayers == 1) && (turn == false))){
+		compplacepiece(p);
+		}
+		turn = !turn;
+		printmat();
+		if (whichp) {
+		p = p2;
+		whichp = false;
+		}
+		else {
+		p=p1;
+		whichp = true;
+		}
+		fullsqrs+=1;
+		}
+		ckwin();
+
+	}
---
+++ Lab2/Player.cpp
@@ -0,0 +1,15 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: Jun 17, 2021
+ *      Author: Treah
+ */
+
+#include "Player.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+char name = '';
+const char black = 'B';
+const char white = 'W';
